1c2c8d1c6ac453c4f481972ca9cc4d0b35eb37e1
tanabe1105 <asutoronn6521@gmail.com>
2026-02-17 12:02:49 +0900

    何のcmd_velを取るか選択できるようにしました

--- imrc_cmd_vel_selector/cmd_vel_selector.py
+++ imrc_cmd_vel_selector/cmd_vel_selector.py
@@ -2,7 +2,9 @@ from example_interfaces.srv import AddTwoInts
 
 import rclpy
 from rclpy.node import Node
+import time
 
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 
@@ -10,23 +12,82 @@ class cmd_vel_selector(Node):
 
     def __init__(self):
         super().__init__('cmd_vel_selector')
-        self.sub = self.create_subscription(Twist, '/cmd_vel_pad', self.cmd_vel_selector_callback,10)
-        self.pub = self.create_publisher(Twist,'/cmd_vel_uart',10)
+        
+        # 出力先
+        self.pub = self.create_publisher(Twist,'/cmd_vel_uart', 10)
+        
+        # 現在購読しているsubscriber（最初は None） 
+        self.current_sub = None 
+        self.current_topic = None
 
         self.vel = Twist() 
-        self.vel.linear.x = 0.0
-        self.vel.linear.y = 0.0
-        self.vel.angular.z = 0.0
+        # self.vel.linear.x = 0.0
+        # self.vel.linear.y = 0.0
+        # self.vel.angular.z = 0.0
+        
+        # 値が更新されていなかったとき、すべて0のTwistを出す
+        self.zero_twist = Twist()
+        # 監視周期は0.1秒
+        self.create_timer(0.1, self.watch_timer)
+        self.target_timer = time.time() # current_velがpubされているか
+        self.twist_timer = time.time() # twistがpubされているか
+        # それぞれの値が変化しているか
+        self.target_flag = False
+        self.twist_flag = False
+        
+        # どの値をcmd_vel_uartに流すか
+        self.target_sub = self.create_subscription(String, '/current_vel', self.target_selecter_callback, 10)
+        
+    def target_selecter_callback(self, msg): 
+        new_topic = msg.data.strip() 
+        self.target_timer = time.time()
+        
+        if new_topic == self.current_topic:
+            # self.get_logger().info("読み取るtopicは変化していません")
+            return
+        
+        # 古いsubscriberを破棄
+        if self.current_sub is not None:
+            self.destroy_subscription(self.current_sub)
+            self.get_logger().info(f"古いtopic購読を解除: {self.current_topic}")
+
+        # 新しいトピックを購読
+        self.current_sub = self.create_subscription(Twist, new_topic, self.cmd_vel_selector_callback, 10) 
+        self.current_topic = new_topic
+        self.get_logger().info(f"topic:{new_topic} ") # 新しいトピックを購読 
+        self.twist_timer = time.time()
 
     def cmd_vel_selector_callback(self, msg):
-        self.vel.linear.x = msg.linear.x
-        self.vel.linear.y = msg.linear.y
+        self.twist_timer = time.time()
+        
+        # self.vel.linear.x = msg.linear.x
+        # self.vel.linear.y = msg.linear.y
+        # self.vel.angular.z = msg.angular.z
 
-        self.vel.angular.z= msg.angular.z
+        self.pub.publish(msg) 
+        self.get_logger().info(f'Lx={msg.linear.x} Ly={msg.linear.y} Az={msg.angular.z}')
 
-        self.pub.publish(self.vel) 
-        self.get_logger().info(f'Lx={self.vel.linear.x} Ly={self.vel.linear.y} Az={self.vel.angular.z}')
-
+    def watch_timer(self):
+        now = time.time()
+        
+        # 前回の結果を保持
+        pre_target_flag = self.target_flag
+        # 停止のための変数
+        self.target_flag = (now - self.target_timer < 2.0)
+        self.twist_flag = (now - self.twist_timer < 2.0)
+        
+        # target_flagがTrueからFalseに変わった場合、購読を解除
+        if pre_target_flag and not self.target_flag:
+            if self.current_sub is not None:
+                self.destroy_subscription(self.current_sub)
+                self.get_logger().info(f"/current_velタイムアウト - topic購読を解除: {self.current_topic}")
+                self.current_sub = None
+                self.current_topic = None
+        
+        if self.target_flag == False or self.twist_flag == False:
+            self.get_logger().info("入力が検知されませんでした") 
+            # self.get_logger().info('Lx=0.0 Ly=0.0 Az=0.0')
+            self.pub.publish(self.zero_twist) 
 
 def main():
     rclpy.init()
